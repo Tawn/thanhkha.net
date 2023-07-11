4c926e24bd082903451caca048be0f658b1b0e38
Tawn <tkha@live.com>
2023-07-11 21:29:58 +0900

    experiences - July 11, 2023

--- scripts/content.js
+++ scripts/content.js
@@ -48,19 +48,22 @@ function displayAbout() {
     // Left Div
     var leftDiv = document.createElement('div');
     leftDiv.classList.add('about_div_left');
-    var header = document.createElement('h1');
-    header.textContent = "Hi, I'm Thanh (Tawn) Kha. I'm a Sr Software Engineer.";
-    leftDiv.appendChild(header);
+    var h1 = document.createElement('h1');
+    h1.textContent = "Hi, I'm Thanh Kha.";
+    leftDiv.appendChild(h1);
+    var h3 = document.createElement('h3');
+    h3.textContent = "Sr Software Engineer";
+    leftDiv.appendChild(h3);
 
-    var paragraph = document.createElement('p');
-    paragraph.textContent = "I'm currently working for Lockheed Martin, Space. "
-        + "I've been in Japan since early of 2022 delivering the first LTE-Over-Space Satellite system. "
-        + "I enjoy working in a team environment, developing (code), and taking on new challenges.";
-    leftDiv.appendChild(paragraph);
+    var p1 = document.createElement('p');
+    p1.textContent = "I'm currently working for Lockheed Martin, Space. "
+        + "I've always enjoyed working on technology and I'm very grateful to be working on new breakthrough technology! "
+        + "I'm currently in Japan since early of 2022 delivering the first LTE-Over-Space Satellite system. ";
+    leftDiv.appendChild(p1);
 
-    var paragraph2 = document.createElement('p');
-    paragraph2.textContent = "My plans for the future are open. Please don't hesitate to reach out to me!";
-    leftDiv.appendChild(paragraph2);
+    var p2 = document.createElement('p');
+    p2.textContent = "Please don't hesitate to reach out with any questions.";
+    leftDiv.appendChild(p2);
 
     // Append Left Div
     mainframe.appendChild(leftDiv);
@@ -76,9 +79,57 @@ function displayAbout() {
 
 }
 function displayExperiences() {
-    var header = document.createElement('h1');
-    header.textContent = "Experiences";
-    mainframe.appendChild(header)
+
+        // Main Div
+        var mainDiv = document.createElement('div');
+        mainDiv.classList.add('experiences_main_div');
+
+        // Center Div
+        var centerDiv = document.createElement('div');
+        centerDiv.classList.add('experiences_center_div');
+
+        // Header Div
+        var headerDiv = document.createElement('div');
+        headerDiv.classList.add('experiences_header_div');
+
+        // (EXP-1 - Lockheed Martin) Header
+        var h1 = document.createElement('h1');
+        h1.textContent = "Lockheed Martin, Space";
+        var h3 = document.createElement('h3');
+        h3.textContent = "Sr Software Engineer (2019 - Current)";
+
+        // Append Headers to Header Div
+        headerDiv.appendChild(h1);
+        headerDiv.appendChild(h3);
+
+        // Append Header Div to Center Div
+        centerDiv.appendChild(headerDiv);
+
+        // (EXP-1 - Lockheed Martin) Image
+        var image = document.createElement('img');
+        image.src = "/images/utsunomiya.jpg";
+        image.classList.add('experiences_image');
+
+        // (EXP-1 - Lockheed Martin) Description
+        var descDiv = document.createElement('div');
+        descDiv.classList.add('experiences_desc_div');
+        var description = document.createElement('p');
+        description.textContent = "I'm currently in Japan, leading the test plans and executions for the delivery of our product. "
+            + "Everyday is always a challenge to satisfy the customer and execute the plan as quickly and efficiently as possible. "
+            + "The team I lead are mostly newjoins that I train to operate the system and instruct for executions. "
+            + "The best part of all this is being able to travel all around Japan and having this awesome experience with others. "
+        
+        // Append image 
+        centerDiv.appendChild(image);
+        // Append description 
+        descDiv.appendChild(description);
+        centerDiv.appendChild(descDiv);
+
+        // Append Center Div
+        mainDiv.appendChild(centerDiv)
+        // Append Main Div
+        mainframe.appendChild(mainDiv);
+        
 }
 function displayProjects() {
     var header = document.createElement('h1');
