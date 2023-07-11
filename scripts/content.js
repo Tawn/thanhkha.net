44092bf27800bf8d66c368ca1eb3ddf79bb72468
Tawn <tkha@live.com>
2023-07-12 01:44:29 +0900

    experiences - July 11, 2023

--- scripts/content.js
+++ scripts/content.js
@@ -7,10 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // Start Content
     var element = document.getElementById('about');
     element.classList.replace('navbar-item', 'navbar-item-selected')
-    displayAbout();
+    
+    const data = sessionStorage.getItem('selection');
+    if (data != null) 
+        navbarSelect(data)
+    else
+        displayAbout();
 });
 
-function navbarSelect(event, selection) {
+function navbarSelect(selection) {
     // Get all navbar items
     var navItems = document.querySelectorAll('nav ul li a');
     // Clear Selection
@@ -29,15 +34,19 @@ function display(selection) {
     switch (selection) {
         case 'about':
             displayAbout();
+            sessionStorage.setItem('selection', 'about');
             break;
         case 'experiences':
             displayExperiences();
+            sessionStorage.setItem('selection', 'experiences');
             break;
         case 'projects':
             displayProjects();
+            sessionStorage.setItem('selection', 'projects');
             break;
         case 'contact':
             displayContact();
+            sessionStorage.setItem('selection', 'contact');
             break;
         default:
             break;
@@ -56,13 +65,15 @@ function displayAbout() {
     leftDiv.appendChild(h3);
 
     var p1 = document.createElement('p');
-    p1.textContent = "I'm currently working for Lockheed Martin, Space. "
-        + "I've always enjoyed working on technology and I'm very grateful to be working on new breakthrough technology! "
-        + "I'm currently in Japan since early of 2022 delivering the first LTE-Over-Space Satellite system. ";
+    p1.textContent = "I am currently employed at Lockheed Martin, Space, where I have the opportunity to work on cutting-edge technology. "
+        + "I have always had a passion for working with technology, and I feel incredibly grateful to be part of the team involved in developing new breakthroughs. "
+        + "I've been in Japan since early 2022, working on delivering the first LTE-Over-Space Satellite system. "
+        + "It's an amazing feeling to see many years of dedicated work prior to now seeing the system in action that will "
+        + "serve a whole country and save lives. ";
     leftDiv.appendChild(p1);
 
     var p2 = document.createElement('p');
-    p2.textContent = "Please don't hesitate to reach out with any questions.";
+    p2.textContent = "Please feel free to reach out to me with any questions.";
     leftDiv.appendChild(p2);
 
     // Append Left Div
@@ -98,7 +109,7 @@ function displayExperiences() {
         var h3 = document.createElement('h3');
         h3.textContent = "Sr Software Engineer (2019 - Current)";
 
-        // Append Headers to Header Div
+        // Append Headers to Header Divx    
         headerDiv.appendChild(h1);
         headerDiv.appendChild(h3);
 
@@ -110,19 +121,30 @@ function displayExperiences() {
         image.src = "/images/utsunomiya.jpg";
         image.classList.add('experiences_image');
 
+        // Append image 
+        centerDiv.appendChild(image);
+
         // (EXP-1 - Lockheed Martin) Description
         var descDiv = document.createElement('div');
         descDiv.classList.add('experiences_desc_div');
-        var description = document.createElement('p');
-        description.textContent = "I'm currently in Japan, leading the test plans and executions for the delivery of our product. "
-            + "Everyday is always a challenge to satisfy the customer and execute the plan as quickly and efficiently as possible. "
-            + "The team I lead are mostly newjoins that I train to operate the system and instruct for executions. "
-            + "The best part of all this is being able to travel all around Japan and having this awesome experience with others. "
+        var desc1 = document.createElement('p');
+        desc1.textContent = "I'm currently in Japan, leading the test plans and executions for the delivery of our product. "
+            + "Every day is always a challenge to satisfy the customer and execute the plan as quickly and efficiently as possible. "
+            + "The team I lead consists mostly of new members whom I train to operate the system and guide in executing tasks. "
+            + "I am considered the SME (Subject-Matter Expert) on the UE (User Equipment) and the overall system, "
+            + "so I directly handle the customers' daily technical questions. "
+            + " The best part of all this is being able to travel all around Japan sharing this experience with others. "
+        var desc2 = document.createElement('p');
+        desc2.textContent = "I automate many of the repetitive tasks I encounter on a daily basis by creating software tools. "
+            + "Since our system is Linux-based, automating system operations is typically accomplished through bash or Python scripts. "
+            + "Another tool I developed, called 'directors_tool,' is specifically designed for automating daily tasks and generating summaries. "
+            + "I have shared this tool on my Projects page. "
+            + "'directors_tool' saves me a significant amount of time when documenting tasks and allows me to easily track their statuses. "
+            + "Building various tools has become essential to manage the numerous moving parts involved in my everyday responsibilities." 
         
-        // Append image 
-        centerDiv.appendChild(image);
         // Append description 
-        descDiv.appendChild(description);
+        descDiv.appendChild(desc1);
+        descDiv.appendChild(desc2);
         centerDiv.appendChild(descDiv);
 
         // Append Center Div
